4bc18d77de9a19216d9c1b68dc99d22973e56d71
Tara Fenton <tarafenton@Taras-MBP-2.fios-router.home>
2018-05-06 19:40:08 -0400

    App.js

--- WhatsThat/App.js
+++ WhatsThat/App.js
@@ -1,58 +1,42 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
+ * shoTel
  */
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, { Component } from "react";
+import { StackNavigator } from "react-navigation";
+import HomeScreen from './components/HomeScreen';
+import CameraScreen from './components/CameraScreen';
+import TellScreen from './components/TellScreen';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+export default StackNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+        headerTitle: "What's That?",
+        headerTintColor: '#BD2D87',
+        headerStyle: {
+            backgroundColor: '#B191FF'
+        }
+    }
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  CameraScreen: {
+    screen: CameraScreen,
+    navigationOptions: {
+        headerTitle: "What's That?",
+        headerTintColor: '#BD2D87',
+        headerStyle: {
+            backgroundColor: '#B191FF'
+        }
+    }
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  TellScreen: {
+    screen: TellScreen,
+    navigationOptions: {
+        headerTitle: "What's That?",
+        headerTintColor: '#BD2D87',
+        headerStyle: {
+            backgroundColor: '#B191FF'
+        }
+    }
   },
 });
