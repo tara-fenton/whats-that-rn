5b1c11b6051cd73d1f4bc96befd938a325e1bc26
Tara Fenton <tarafenton@Taras-MBP-2.fios-router.home>
2018-05-06 20:34:21 -0400

    new app is back to where experiment left off

--- WhatsThat/android/app/src/main/java/com/whatsthat/MainApplication.java
+++ WhatsThat/android/app/src/main/java/com/whatsthat/MainApplication.java
@@ -3,6 +3,7 @@ package com.whatsthat;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new RNFetchBlobPackage(),
             new RNCameraPackage()
       );
