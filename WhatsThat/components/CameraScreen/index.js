896fa4e1b291923f8b83c8872ef6f26fd258e61d
Tara Fenton <tarafenton@Taras-MBP-2.fios-router.home>
2018-05-06 20:39:50 -0400

    clean CameraScreen code

--- WhatsThat/components/CameraScreen/index.js
+++ WhatsThat/components/CameraScreen/index.js
@@ -6,12 +6,9 @@
 
 import React, { Component } from "react";
 import {
-  Platform,
   StyleSheet,
   Text,
   View,
-  TouchableOpacity,
-  Button,
   Dimensions,
   TouchableHighlight,
   Image
@@ -28,115 +25,11 @@ export default class HomeScreen extends Component {
   }
   componentDidMount() {
     process.nextTick = setImmediate;
-    //this.fetchData();
-    //this.createStringOfNames();
   }
   componentWillUnmount() {
     console.log('camera unmounted');
   }
-  // this works but want to try something else
-  // render() {
-  //   return (
-  //     <View style={styles.container}>
-  //       <Camera
-  //         ref={cam => (this.camera = cam)}
-  //         aspect={Camera.constants.Aspect.fill}
-  //         type={Camera.constants.Type.back}
-  //         style={styles.preview}
-  //         flashMode={Camera.constants.FlashMode.on}
-  //         //captureTarget={RNCamera.constants.CaptureTarget.disk}
-  //         // captureTarget={Camera.constants.CaptureTarget.temp}
-  //         // captureTarget={Camera.constants.CaptureTarget.memory}
-  //         permissionDialogTitle={"Permission to use camera"}
-  //         permissionDialogMessage={
-  //           "We need your permission to use your camera phone"
-  //         }
-  //       />
-  //       {/* <Image
-  // source={{uri: this.state.baseImage, isStatic:true}}
-  // style={{width: 100, height: 100}}
-  // /> */}
-  //
-  //       <TouchableOpacity>
-  //         <View style={styles.buttonContainer}>
-  //           <Button
-  //             onPress={this.sendPicture.bind(this)}
-  //             title="Send Picture"
-  //             color="#841584"
-  //             accessibilityLabel="Send Picture"
-  //           />
-  //         </View>
-  //       </TouchableOpacity>
-  //       <TouchableOpacity>
-  //         <View style={styles.buttonContainer}>
-  //           <Button
-  //             onPress={this.takePicture.bind(this)}
-  //             title="Take Picture"
-  //             color="#841584"
-  //             accessibilityLabel="Take Picture"
-  //           />
-  //         </View>
-  //       </TouchableOpacity>
-  //     </View>
-  //   );
-  // }
-  // takePicture = async function() {
-  //   if (this.camera) {
-  //     const options = { quality: 0.5, base64: true };
-  //     const data = await this.camera.capture(options);
-  //     console.log("this is called");
-  //     console.log(data);
-  //     //console.log(this.camera.saveImage());
-  //
-  //     // let photo = await this.camera.takePictureAsync();
-  //     //       console.log(photo);
-  //     this.setState({
-  //       baseImage: data.path
-  //     });
-  //     //RNFetchBlob.config({ path : RNFetchBlob.fs.dirs.DocumentDir + '/userThumbnails/user.png' +  })
-  //   // const dirs = RNFetchBlob.fs.dirs
-  //   //
-  //   // console.log('BLOB   ',dirs);
-  //   // console.log('BLOB   ',RNFetchBlob.fs.dirs);
-  //   // const saved = await this.camera.saveImage();
-  //   //   console.log(saved);
-  //     // const data = await this.camera.capture();
-  //     //CameraRoll.saveToCameraRoll( data.uri )
-  //     //console.log("where the path ",this.camera.constants.CaptureTarget.memory);
-  //     // captureTarget={Camera.constants.CaptureTarget.memory}
-  //
-  //   }
-  // };
-  // sendPicture = async function() {
-  //   if (this.camera) {
-  //     console.log("sendPicture is called");
-  //     console.log(this.state.baseImage);
-  //     // convert the image to base64 data
-  //     RNFetchBlob.fs
-  //       .readFile(this.state.baseImage, "base64")
-  //       .then(data => {
-  //         app.models
-  //           .predict(Clarifai.GENERAL_MODEL, { base64: data })
-  //           .then(response => {
-  //             // do something with response
-  //             console.log("response from clarifai ", response);
-  //             this.setState({
-  //               data: response,
-  //               conceptsLoaded: true
-  //             });
-  //             this.createStringOfNames();
-  //           });
-  //         // function(err) {
-  //         //   // there was an error
-  //         // }
-  //         //);
-  //       })
-  //       .catch(error => {
-  //         // error on converting image to base64 data
-  //         console.log(error);
-  //       });
-  //   }
-  // };
+
   takePicture() {
     this.camera
       .capture()
@@ -194,7 +87,7 @@ export default class HomeScreen extends Component {
       path: this.state.path
     })
   }
-  
+
   render() {
     return (
       <View style={styles.container}>
